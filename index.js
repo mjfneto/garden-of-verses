4634c1488dcc681b7b398ba0deb326c5fa09f03a
mjfneto <mjfneto@gmail.com>
2025-04-29 19:18:12 -0300

    refactor: isolate form submit and change logic into dedicated handler functions

--- index.js
+++ index.js
@@ -6,47 +6,48 @@ import {
 } from './src/ui/poemsManager.js'
 
 const searchForm = document.querySelector(SELECTORS.searchForm)
-
 const listControlsForm = document.querySelector(SELECTORS.listControls)
 
 export let searchFormEntries = {}
 
-searchForm.addEventListener('submit', function (event) {
+function onSearchSubmit(event) {
   event.preventDefault()
 
   const searchFormData = new FormData(searchForm)
   searchFormEntries = Object.fromEntries(searchFormData.entries())
-  let inputFields = []
-  let searchTerms = []
 
-  if (Object.values(searchFormEntries).every((value) => !value)) {
+  if (Object.values(searchFormEntries).every((val) => !val)) {
     alert('Kindly fill out at least one field before you submit the form.')
     return
   }
 
-  for (const [inputField, searchTerm] of Object.entries(searchFormEntries)) {
-    if (searchFormEntries.random && inputField === 'poemcount') {
-      continue
-    }
+  const { inputFields, searchTerms } = buildSearchParams(searchFormEntries)
+  loadPoems(`${inputFields.join(',')}/${searchTerms.join(';')}`)
+}
 
-    if (searchTerm) {
-      inputFields.push(inputField)
-      if (inputField === 'random') {
-        if (searchFormEntries.poemcount) {
-          searchTerms.push(searchFormEntries.poemcount)
-        } else {
-          searchTerms.push('1')
-        }
-      } else {
-        searchTerms.push(searchTerm)
-      }
+function onListControlsChange(event) {
+  handleCheckboxes(event)
+  updatePoems()
+}
+
+export function buildSearchParams(entries) {
+  let inputFields = []
+  let searchTerms = []
+
+  for (const [field, term] of Object.entries(entries)) {
+    if (entries.random && field === 'poemcount') continue
+    if (!term) continue
+
+    inputFields.push(field)
+    if (field === 'random') {
+      searchTerms.push(entries.poemcount || '1')
+    } else {
+      searchTerms.push(term)
     }
   }
 
-  loadPoems(`${inputFields.join(',')}/${searchTerms.join(';')}`)
-})
+  return { inputFields, searchTerms }
+}
 
-listControlsForm.addEventListener('change', function (event) {
-  handleCheckboxes(event)
-  updatePoems()
-})
+searchForm.addEventListener('submit', onSearchSubmit)
+listControlsForm.addEventListener('change', onListControlsChange)
